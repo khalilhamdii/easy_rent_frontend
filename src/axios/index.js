7988a90871ebb66ad6672211568c1f116e7bf77c
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-25 04:20:37 +0100

    Fix linter errors

--- src/axios/index.js
+++ src/axios/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const apiLogin = (user, action, redirect, setErrors) => {
   axios
     .post('https://easyrent-backend.herokuapp.com/login', { user })
-    .then((response) => {
+    .then(response => {
       if (response.data.logged_in) {
         action(response.data);
         redirect();
@@ -12,13 +12,13 @@ export const apiLogin = (user, action, redirect, setErrors) => {
         setErrors(response.data.errors);
       }
     })
-    .catch((error) => console.log('api errors:', error.response.data));
+    .catch(error => console.log('api errors:', error.response.data));
 };
 
 export const apiSignUp = (user, action, redirect, setErrors) => {
   axios
     .post('https://easyrent-backend.herokuapp.com/users', { user })
-    .then((response) => {
+    .then(response => {
       if (response.data.status === 'created') {
         action(response.data);
         redirect();
@@ -26,94 +26,94 @@ export const apiSignUp = (user, action, redirect, setErrors) => {
         setErrors(response.data.errors);
       }
     })
-    .catch((error) => console.log('api errors:', error.response.data));
+    .catch(error => console.log('api errors:', error.response.data));
 };
 
 export const apiLogOut = (user, action) => {
   axios
     .post('https://easyrent-backend.herokuapp.com/logout', { user })
-    .then((response) => {
+    .then(response => {
       if (response.data.logged_out) {
         action();
       }
     })
-    .catch((error) => console.log('api errors:', error));
+    .catch(error => console.log('api errors:', error));
 };
 
 export const apiGetLoginStatus = (loginHandler, logoutHandler) => {
   axios
     .get('https://easyrent-backend.herokuapp.com/logged_in')
-    .then((response) => {
+    .then(response => {
       if (response.data.logged_in) {
         loginHandler(response.data);
       } else {
         logoutHandler();
       }
     })
-    .catch((error) => console.log('api errors:', error));
+    .catch(error => console.log('api errors:', error));
 };
 
 export const apiAddRent = (rent, userId, action) => {
   axios
     .post(`https://easyrent-backend.herokuapp.com/users/${userId}/rents/`, rent)
-    .then((response) => {
+    .then(response => {
       if (response.data.status === 'created') {
         action(rent);
       }
     })
-    .catch((error) => console.log('api errors:', error.response.data));
+    .catch(error => console.log('api errors:', error.response.data));
 };
 
 export const apiRemoveRent = (id, action) => {
   axios
     .delete(`https://easyrent-backend.herokuapp.com/rents/${id}`)
-    .then((response) => {
+    .then(response => {
       if (response.data.status === 'deleted') {
         action(id);
       }
     })
-    .catch((error) => console.log('api errors:', error));
+    .catch(error => console.log('api errors:', error));
 };
 
 export const apiChangeRentStatus = (id, rent, status, action) => {
   const modifiedRent = { ...rent, status };
   axios
     .patch(`https://easyrent-backend.herokuapp.com/rents/${id}`, modifiedRent)
-    .then((response) => {
+    .then(response => {
       if (response.data.status === 'updated') {
         action(id, status);
       }
     })
-    .catch((error) => console.log('api errors:', error));
+    .catch(error => console.log('api errors:', error));
 };
 
-export const apiGetRents = (action) => {
+export const apiGetRents = action => {
   axios
     .get('https://easyrent-backend.herokuapp.com/rents/')
-    .then((response) => {
+    .then(response => {
       action(response.data);
     })
-    .catch((error) => console.log('api errors:', error));
+    .catch(error => console.log('api errors:', error));
 };
 
 export const apiGetUserRents = (id, action) => {
   axios
     .get(`https://easyrent-backend.herokuapp.com/users/${id}/rents/`)
-    .then((response) => {
+    .then(response => {
       action(response.data);
     })
-    .catch((error) => console.log('api errors:', error.response.data));
+    .catch(error => console.log('api errors:', error.response.data));
 };
 
-export const apiGetCars = (action) => {
+export const apiGetCars = action => {
   axios
     .get('https://easyrent-backend.herokuapp.com/api/v1/cars')
-    .then((response) => {
+    .then(response => {
       if (response.status === 200) {
         action(response.data);
       }
     })
-    .catch((error) => console.log('api errors:', error.response.data));
+    .catch(error => console.log('api errors:', error.response.data));
 };
 
 export const apiAddCar = (carData, action) => {
@@ -121,32 +121,32 @@ export const apiAddCar = (carData, action) => {
     .post('https://easyrent-backend.herokuapp.com/api/v1/cars', carData, {
       headers: { 'content-type': 'multipart/form-data' },
     })
-    .then((response) => {
+    .then(response => {
       if (response.statusText === 'Created') {
         action(response.data);
       }
     })
-    .catch((error) => console.log('api errors:', error.response.data));
+    .catch(error => console.log('api errors:', error.response.data));
 };
 
 export const apiRemoveCar = (id, action) => {
   axios
     .delete(`https://easyrent-backend.herokuapp.com/api/v1/cars/${id}`)
-    .then((response) => {
+    .then(response => {
       if (response.data.status === 'deleted') {
         action(id);
       }
     })
-    .catch((error) => console.log('api errors:', error));
+    .catch(error => console.log('api errors:', error));
 };
 
 export const apiEditCar = (id, carData, action) => {
   axios
     .patch(`https://easyrent-backend.herokuapp.com/api/v1/cars/${id}`, carData)
-    .then((response) => {
+    .then(response => {
       if (response.statusText === 'Accepted') {
         action(response.data);
       }
     })
-    .catch((error) => console.log('api errors:', error));
+    .catch(error => console.log('api errors:', error));
 };
